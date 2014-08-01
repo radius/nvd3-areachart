3a6752ffa4ce7867a16b5ba1f10637f4d4019d6f
Darius <darius@artivest.co>
2014-08-01 01:40:57 -0400

    intial commit

---
+++ js/AreaChart.js
@@ -0,0 +1,36 @@
+var AreaChart = function(selector) {
+  this.selector = selector;
+};
+AreaChart.prototype.show = function() {
+  var self = this;
+  d3.json('data/points.json', function(data) {
+    nv.addGraph(function() {
+      var chart = nv.models.lineChart()
+        .x(function(d) {
+            return d[0]
+        })
+        .y(function(d) {
+            return d[1]
+        })
+        .clipEdge(true)
+        .useInteractiveGuideline(true)
+        .transitionDuration(500)
+        .showLegend(false);
+
+      chart.xAxis
+        .showMaxMin(false)
+        .tickFormat(function(d) {
+            return d3.time.format('%x')(new Date(d))
+        });
+
+      chart.yAxis.tickFormat(d3.format(',.2f'));
+
+      d3.select(self.selector)
+        .datum(data)
+        .transition().duration(500).call(chart);
+
+      nv.utils.windowResize(chart.update);
+      return chart;
+    });
+  });  
+}
